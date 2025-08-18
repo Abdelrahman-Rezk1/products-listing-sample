98cb8d3869c21a47ea69766d6ed0e678d27198de
Abdelrahman Rezk-1 <abdelrahman.rezk.elmohamady@outlook.com>
2025-08-18 15:09:32 +0300

    refactor(config): enhance Zoho configuration by using registerAs and defining interface for type safety

--- src/configs/zoho.config.ts
+++ src/configs/zoho.config.ts
@@ -1,8 +1,21 @@
-export default () => ({
-    accounts: process.env.ZOHOSA_ACCOUNTS,
-    clientID: process.env.ZOHO_CLIENT_ID,
-    secret: process.env.ZOHO_CLIENT_SECRET,
-    redirectURI: process.env.ZOHO_REDIRECT_URI,
-    scopes: process.env.ZOHO_SCOPES,
-});
-  
+// src/configs/zoho.config.ts
+import { registerAs } from '@nestjs/config';
+
+export interface ZohoConfig {
+  accounts: string;
+  clientID: string;
+  secret: string;
+  redirectURI: string;
+  scopes: string;
+}
+
+export default registerAs(
+  'zoho',
+  (): ZohoConfig => ({
+    accounts: process.env.ZOHOSA_ACCOUNTS!, // non-null because Joi validated
+    clientID: process.env.ZOHO_CLIENT_ID!,
+    secret: process.env.ZOHO_CLIENT_SECRET!,
+    redirectURI: process.env.ZOHO_REDIRECT_URI!,
+    scopes: process.env.ZOHO_SCOPES!,
+  }),
+);

11ef38952085637466e4da415b65ef807b392c10
Abdelrahman Rezk-1 <abdelrahman.rezk.elmohamady@outlook.com>
2025-08-18 15:55:18 +0300

    feat(auth): register AuthController and AuthService in AuthModule for improved authentication handling

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,10 @@
-
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ],
-  controllers: [],
-  providers: [],
-  exports: [],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AuthModule {}

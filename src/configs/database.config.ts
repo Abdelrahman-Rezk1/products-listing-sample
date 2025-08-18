179067a06db5dcd7f8d7b4e0e09131b510bd8e8f
Abdelrahman Rezk-1 <abdelrahman.rezk.elmohamady@outlook.com>
2025-08-18 15:11:48 +0300

    refactor(configs): update database configuration to use registerAs and define DbConfig interface for improved type safety

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -1,8 +1,23 @@
-export default () => ({
-  type: process.env.DB_TYPE,
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  synchronize: true,
-});
+import { registerAs } from '@nestjs/config';
+
+export interface DbConfig {
+  type: string;
+  host: string;
+  username: string;
+  password: string;
+  database: string;
+  synchronize: boolean;
+}
+
+export default registerAs(
+  'db',
+  (): DbConfig => ({
+    // Non-null assertions are safe because Joi will validate at bootstrap
+    type: process.env.DB_TYPE!,
+    host: process.env.DB_HOST!,
+    username: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_DATABASE!,
+    synchronize: true,
+  }),
+);
